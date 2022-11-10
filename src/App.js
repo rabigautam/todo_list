d71ef7796b5c6b155e57597d381776bbcca948bb
Rabi Gautam <rabi@sharelookapp.com>
2022-11-10 08:18:10 +0545

    feat:add filter for listing

--- src/App.js
+++ src/App.js
@@ -20,7 +20,27 @@ function App(props) {
             Add
           </button>
         </form>
-  
+        <div className="filters btn-group stack-exception">
+          <button type="button" className="btn toggle-btn" aria-pressed="false">
+            <span className="visually-hidden">Show </span>
+            <span>all</span>
+            <span className="visually-hidden"> tasks</span>
+          </button>
+          <button type="button" className="btn toggle-btn" aria-pressed="false">
+            <span className="visually-hidden">Show </span>
+            <span>Active</span>
+            <span className="visually-hidden"> tasks</span>
+          </button>
+          <button type="button" className="btn toggle-btn" aria-pressed="false">
+            <span className="visually-hidden">Show </span>
+            <span>Completed</span>
+            <span className="visually-hidden"> tasks</span>
+          </button>
+        </div>
+        <h2 id="list-heading">
+          3 tasks remaining
+        </h2>
+       
       </div>
     );
   }

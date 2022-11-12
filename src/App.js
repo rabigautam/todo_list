0cd8060be324ee6f99e68bbcaeb022cdff8cb04f
Rabi Gautam <rabi@sharelookapp.com>
2022-11-12 19:54:04 +0545

    feat:interactivity form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 
 import FilterButton from "./components/FilterButton";
-
+function addTask(task){
+    alert(`${task}`)
+}
 function App(props) {
   const taskList = props.tasks?.map((task) => (
     <Todo key = {task.id} id={task.id} name={task.name} completed={task.completed} />
@@ -12,7 +14,7 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <FilterButton/>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Form (props){
-    return(
-<form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-    );
+export default function Form(props) {
+  const [name, setName] = useState(" ");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e, "event");
+    if (name.trim().length ) {
+      props.addTask(name);
+      setName("");
+    }
+  }
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }

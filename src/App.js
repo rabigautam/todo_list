1ce46bd63a526b8cce802a1bf0e4ff2283ea32e4
Rabi Gautam <rabi@sharelookapp.com>
2022-11-13 09:33:51 +0545

    feat:add task and manage toggle for check box

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
-
+import { nanoid } from "nanoid";
 import FilterButton from "./components/FilterButton";
-function addTask(task){
-    alert(`${task}`)
-}
+
 function App(props) {
-  const taskList = props.tasks?.map((task) => (
-    <Todo key = {task.id} id={task.id} name={task.name} completed={task.completed} />
+  const [tasks, setTasks] = useState(props.tasks);
+  function addTask(task) {
+    const newTask = { id: `todo-${nanoid()}`, name: task, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  function toggleTaskCompleted(id) {
+    const updatedTask = tasks.map((task) => {
+      if (id === task.id) {
+        task = {
+          ...task,
+          completed: !task.completed
+        };
+        console.log(task);
+        return task;
+      }
+      return task;
+    });
+    setTasks(updatedTask);
+  }
+  const taskList = tasks?.map((task) => (
+    <Todo
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      toggleTaskCompleted={toggleTaskCompleted}
+    />
   ));
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaning`;
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form addTask={addTask}/>
-      <FilterButton/>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <Form addTask={addTask} />
+      <FilterButton />
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
